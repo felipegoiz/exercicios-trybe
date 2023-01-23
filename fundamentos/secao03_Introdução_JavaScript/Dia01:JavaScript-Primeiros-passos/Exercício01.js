fa2e528af7d7438479df1d7ee5731b81a82b9a9c
Felipe Goiz <felipegoiz@hotmail.com>
2023-01-23 12:12:55 -0300

    Finalizando a terceira atividade do para fixar

--- fundamentos/secao03_Introdução_JavaScript/Dia01:JavaScript-Primeiros-passos/Exercício01.js
+++ fundamentos/secao03_Introdução_JavaScript/Dia01:JavaScript-Primeiros-passos/Exercício01.js
@@ -1,26 +1,44 @@
-// PRIMEIRO EXERCICIO
+// // PRIMEIRO EXERCICIO
 
-// const myName = "Felipe Goiz";
+// // const myName = "Felipe Goiz";
 
-// const birthCity = 33;
-// let birthYear = 1989;
+// // const birthCity = 33;
+// // let birthYear = 1989;
 
-// console.log(myName);
-// console.log(birthCity);
-// console.log(birthYear);
+// // console.log(myName);
+// // console.log(birthCity);
+// // console.log(birthYear);
 
-// birthYear = 2030;
+// // birthYear = 2030;
 
-// console.log(birthYear);
+// // console.log(birthYear);
 
-// SEGUNDO EXERCICIO
+// // SEGUNDO EXERCICIO
 
-const base = 5;
-const heigth = 8;
-const area = base * heigth;
+// // const base = 5;
+// // const heigth = 8;
+// // const area = base * heigth;
 
-console.log(area);
+// // console.log(area);
 
-const perimeter = 2 * base + 2 * heigth;
+// // const perimeter = 2 * base + 2 * heigth;
 
-console.log(perimeter);
+// // console.log(perimeter);
+
+// // TERCEIRO EXERCICIO
+
+// const nota = 70;
+
+// if (nota >= 80 && nota <= 100) {
+//   console.log("Parabéns você faz parte do grupo das pessoas aprovadas!");
+// }
+
+// else if (nota < 80 && nota >= 60) {
+//   console.log("Você está na nossa lista de espera.");
+// }
+
+// else {
+//   console.log("Infelizmente, você reprovou.");
+// }
+
+// QUARTO EXERCICIO
