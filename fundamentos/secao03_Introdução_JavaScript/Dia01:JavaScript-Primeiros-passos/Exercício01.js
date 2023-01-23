69ab73ff2d160633ab9d66e980549a3b177c6634
Felipe Goiz <felipegoiz@hotmail.com>
2023-01-23 12:01:40 -0300

    Finalizando a segunda atividade do para fixar

--- fundamentos/secao03_Introdução_JavaScript/Dia01:JavaScript-Primeiros-passos/Exercício01.js
+++ fundamentos/secao03_Introdução_JavaScript/Dia01:JavaScript-Primeiros-passos/Exercício01.js
@@ -1,11 +1,26 @@
-const myName = "Felipe Goiz";
-const birthCity = 33;
-let birthYear = 1989;
+// PRIMEIRO EXERCICIO
 
-console.log(myName);
-console.log(birthCity);
-console.log(birthYear);
+// const myName = "Felipe Goiz";
 
-birthYear = 2030;
+// const birthCity = 33;
+// let birthYear = 1989;
 
-console.log(birthYear);
+// console.log(myName);
+// console.log(birthCity);
+// console.log(birthYear);
+
+// birthYear = 2030;
+
+// console.log(birthYear);
+
+// SEGUNDO EXERCICIO
+
+const base = 5;
+const heigth = 8;
+const area = base * heigth;
+
+console.log(area);
+
+const perimeter = 2 * base + 2 * heigth;
+
+console.log(perimeter);
