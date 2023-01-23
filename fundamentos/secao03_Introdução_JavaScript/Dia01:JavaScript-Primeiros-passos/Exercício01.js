296b504ef268101cc1158ee44ad4524f9f5b15ab
Felipe Goiz <felipegoiz@hotmail.com>
2023-01-23 11:50:34 -0300

    Finalizando a primeira atividade do para fixar

---
+++ fundamentos/secao03_Introdução_JavaScript/Dia01:JavaScript-Primeiros-passos/Exercício01.js
@@ -0,0 +1,11 @@
+const myName = "Felipe Goiz";
+const birthCity = 33;
+let birthYear = 1989;
+
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
+
+birthYear = 2030;
+
+console.log(birthYear);
