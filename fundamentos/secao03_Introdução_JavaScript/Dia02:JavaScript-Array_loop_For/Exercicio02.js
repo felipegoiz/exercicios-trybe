212e55bcd91a1a07f512d0fe8a419adc8035c5c3
Felipe Goiz <felipegoiz@hotmail.com>
2023-01-24 16:07:57 -0300

    Terminando mais alguns exercicios

--- fundamentos/secao03_Introdução_JavaScript/Dia02:JavaScript-Array_loop_For/Exercicio02.js
+++ fundamentos/secao03_Introdução_JavaScript/Dia02:JavaScript-Array_loop_For/Exercicio02.js
@@ -1,11 +1,41 @@
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-for(let index = 0; index<numbers.length;index+=1){
-  console.log(numbers[index]);
-}
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// for(let index = 0; index<numbers.length;index+=1){
+//   console.log(numbers[index]);
+// }
+
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// let result = 0;
+// for (let index = 0 ; index < numbers.length ; index += 1){
+//   result += numbers[index];
+// }
+// console.log(result);
+
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// let result = 0;
+// for (let index = 0 ; index < numbers.length ; index += 1){
+//   result += numbers[index];
+// }
+// console.log(result/10);
+
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// let result = 0;
+// for (let index = 0 ; index < numbers.length ; index += 1){
+//   result += numbers[index];
+// }
+// let media = result / numbers.length;
+// console.log(media);
+// if(media>20){
+//   console.log("valor maior que 20.")
+// } else{
+//   console.log("valor menor ou igual a 20.")
+// }
 
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let result = 0;
-for (let index = 0 ; index < numbers.length ; index += 1){
-  result += numbers[index];
+let maioNumero = numbers[0];
+
+for(let index = 1; index<numbers.length;index+=1){
+  if (numbers[index]> maioNumero){
+    maioNumero = numbers[index];
+  }
 }
-console.log(result);
+console.log(maioNumero);
