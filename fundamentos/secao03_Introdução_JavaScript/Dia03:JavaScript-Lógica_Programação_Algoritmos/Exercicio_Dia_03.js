b5efa001fc0eaf84b5c1e678220b6691e128031b
Felipe Goiz <felipegoiz@hotmail.com>
2023-01-26 15:10:53 -0300

    Finalizando as atividades

--- fundamentos/secao03_Introdução_JavaScript/Dia03:JavaScript-Lógica_Programação_Algoritmos/Exercicio_Dia_03.js
+++ fundamentos/secao03_Introdução_JavaScript/Dia03:JavaScript-Lógica_Programação_Algoritmos/Exercicio_Dia_03.js
@@ -8,3 +8,29 @@
 // };
 // console.log(resultado)
 
+// const nomes = 'Tryber';
+// let resultado = '';
+// for (let index = 0; index < nomes.length; index += 1) {
+//   resultado += nomes[nomes.length - 1 - index];
+// };
+// console.log(resultado);
+
+let array = ['java', 'javascript', 'python', 'html', 'css'];
+
+let biggestWord = array[0];
+let smallestWord = array[0];
+
+for (let index = 0; index < array.length; index += 1) {
+  if (array[index].length > biggestWord.length) {
+    biggestWord = array[index];
+  }
+}
+
+for (let index = 0; index < array.length; index += 1) {
+  if (array[index].length < smallestWord.length) {
+    smallestWord = array[index];
+  }
+}
+
+console.log(biggestWord);
+console.log(smallestWord);
