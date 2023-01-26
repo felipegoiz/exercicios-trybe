92c3702c43b9fc3373d632cb186fb23cf3d5b3aa
Felipe Goiz <felipegoiz@hotmail.com>
2023-01-26 15:30:24 -0300

    Aprendendo um pouco mais dos objetos

---
+++ fundamentos/secao03_Introdução_JavaScript/Dia04:JavaScript-Objetos_funções/Exercicios_dia_04.js
@@ -0,0 +1,16 @@
+let player = {
+  nome: 'Marta',
+  lastName: 'Silva',
+  age: 34,
+  medals: {
+    golden: 2,
+    silver: 3,
+  },
+}
+
+player.bestInTheWorld = [ 2006, 2007, 2008, 2009, 2010,  2018];
+
+  console.log('A jogadora '+ player.nome +' '+ player.lastName +' tem '+ player.age +' anos de idade');
+
+  
+  console.log('A jogadora '+ player.nome +' '+ player.lastName + ' foi eleita a melhor do mundo por '+ player.bestInTheWorld.length +' vezes, os anos foram: '+ player.bestInTheWorld);
